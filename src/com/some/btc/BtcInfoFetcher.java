ed85660ea444e8f6123a1b8540c4fe91d0913856
maqiang <maqiangddb@gmail.com>
2013-12-10 08:55:02 +0800

    first commit

---
+++ src/com/some/btc/BtcInfoFetcher.java
@@ -0,0 +1,105 @@
+package com.some.btc;
+
+import android.util.Log;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * use fetch() method
+ * if you want to know how to use, see Main()
+ * @author xiaochi
+ *
+ */
+public class BtcInfoFetcher {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws JSONException {
+        // test
+    	// also an example
+        BtcInfoFetcher btcInfoFetcher = new BtcInfoFetcher();
+        JSONObject jsonObject = btcInfoFetcher.fetch();
+        System.out.println(jsonObject.toString());
+    }
+    
+    /**
+     * 拉取信息
+     * @return JSONObject
+     */
+    public JSONObject fetch() throws JSONException {
+      String jsonString = fetchRaw();
+      JSONObject jsonObject = new JSONObject(jsonString);
+       Log.e("mq", "fetch data:"+jsonObject.toString());
+      return jsonObject.getJSONObject("ticker");
+    }
+
+    /**
+     * fetch the json string
+     * it may take 30 seconds or more
+     * @return
+     */
+    public String fetchRaw() {
+        double random = Math.random();
+        
+        String url = "http://info.btc123.com/lib/jsonProxyEx.php?type=btcchinaTicker&suffix=" + random;
+        System.out.println(url);
+        
+        HttpClient client = new DefaultHttpClient();
+        HttpGet request = new HttpGet(url);
+        request.addHeader("Referer", "http://info.btc123.com/index_btcchina.php");
+        request.addHeader("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36");
+      
+        HttpResponse response = null;
+            try {
+                response = client.execute(request);
+                Log.e("mq", "!!!!!!!!!!!!!!");
+            } catch (ClientProtocolException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (Exception e) {
+                Log.e("mq","????????????????");
+            }
+        Log.e("mq", "response:"+response);
+        //System.out.println("Response Code: " +
+        //response.getStatusLine().getStatusCode());
+      
+        BufferedReader rd = null;
+            try {
+                rd = new BufferedReader(  
+                    new InputStreamReader(response.getEntity().getContent()));
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }  
+        String line = "";
+        StringBuffer stringBuffer = new StringBuffer();
+        try {
+                while((line = rd.readLine()) != null) {  
+                    System.out.println(line);
+                    stringBuffer.append(line);
+                    }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+    
+        return stringBuffer.toString();
+    }
+}
---
+++ src/com/some/btc/MainActivity.java
@@ -0,0 +1,130 @@
+package com.some.btc;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity
+{
+
+    ListView _dataList;
+    ArrayAdapter _dataAdapter;
+
+    String[] KEYS = {
+            "platform",
+            "last",
+            "buy",
+            "sell",
+            "vol"
+    };
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        initUI();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        try {
+            fetchData();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void initUI() {
+        setContentView(R.layout.main);
+        _dataList = (ListView) findViewById(R.id.data_list);
+        _dataAdapter = new DataList(this, R.layout.list_item);
+        _dataList.setAdapter(_dataAdapter);
+    }
+
+    private void fetchData() throws JSONException {
+        BtcInfoFetcher fetcher = new BtcInfoFetcher();
+        JSONObject jsonObject = fetcher.fetch();
+        ArrayList values = new ArrayList();
+        DataObject btc_china = new DataObject(
+                "btc china",
+                jsonObject.getString("last"),
+                jsonObject.getString("buy"),
+                jsonObject.getString("sell"),
+                jsonObject.getString("vol")
+        );
+        _dataAdapter.add(btc_china);
+    }
+
+    private class DataList extends ArrayAdapter<DataObject> {
+
+        private int _itemLayout;
+
+        public DataList(Context context, int textViewResourceId) {
+            super(context, textViewResourceId);
+            _itemLayout = textViewResourceId;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            DataObject data = getItem(position);
+            View result = inflater.inflate(_itemLayout, new LinearLayout(this.getContext()), true);
+            TextView platform = (TextView) result.findViewById(R.id.platform);
+            TextView last_price = (TextView) result.findViewById(R.id.last_price);
+            TextView buy_price = (TextView) result.findViewById(R.id.buy_price);
+            TextView sell_price = (TextView) result.findViewById(R.id.sell_price);
+            TextView volume = (TextView) result.findViewById(R.id.volumn);
+
+            platform.setText(data._platform);
+            last_price.setText(data._lastPrice);
+            buy_price.setText(data._buyPrice);
+            sell_price.setText(data._sellPrice);
+            volume.setText(data._volume);
+            return result;
+        }
+    }
+
+    private class DataObject {
+        public String _platform;
+        public String _lastPrice;
+        public String _buyPrice;
+        public String _sellPrice;
+        public String _volume;
+
+        DataObject(String platform, String lastPrice, String buyPrice, String sellPrice, String volumn) {
+            _platform = platform;
+            _lastPrice = lastPrice;
+            _buyPrice = buyPrice;
+            _sellPrice = sellPrice;
+            _volume = volumn;
+        }
+
+    }
+
+}
